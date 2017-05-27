66b36b95cb1aa40277555ff9d719835abc90886a
Josue Chinchilla <josue.chinchilla@prodigious.com>
2017-05-27 11:44:58 -0600

    Add temperature, humidity and pressure

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '5aff06b7ce5f7f98ea86fae01212ca48';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},cr`;
+    const request = axios.get(url);
+
+    console.log('Request', request);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
---
+++ src/containers/weather_list.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import Chart from '../components/chart';
+
+
+
+class WeatherList extends Component {
+    
+    renderWeather(cityData)  {
+        const cityName = cityData.city.name;
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+ 
+
+        return (
+            <tr key={cityName}>
+                <td id="city-name">{cityName}</td>
+                <td className="chart-field"> 
+                    <Chart data={pressures} color="green" units="hPa" />
+                </td>
+                <td className="chart-field">
+                    <Chart data={temps} color="orange" units="C" />
+                </td>
+                <td className="chart-field">
+                    <Chart data={humidities} color="red" units="%" />
+                </td>
+
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <div>
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Temperature (C)</th>
+                        <th>Humidity (%)</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps({ weather }) {
+    return { weather };
+}
+
+export default connect(mapStateToProps)(WeatherList);
